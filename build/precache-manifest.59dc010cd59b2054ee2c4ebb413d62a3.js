2d21309abc01b5fb0348cf82ad41911532078f87
Horváth Balázs <tcgmilan@Horvaths-MacBook-Air.local>
2022-03-01 12:09:28 -0800

    Uploading source code

---
+++ build/precache-manifest.59dc010cd59b2054ee2c4ebb413d62a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a0f93e7ec50516cc5bcfb4e26042e3",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "942436f74a4b8bcee8cf",
+    "url": "/todo-app/static/css/main.4e987138.chunk.css"
+  },
+  {
+    "revision": "fe0148b8a82147ece620",
+    "url": "/todo-app/static/js/2.4d4bc6f1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-app/static/js/2.4d4bc6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942436f74a4b8bcee8cf",
+    "url": "/todo-app/static/js/main.050804bb.chunk.js"
+  },
+  {
+    "revision": "32ffbcb42511e8498550",
+    "url": "/todo-app/static/js/runtime-main.2d192a4e.js"
+  }
+]);
